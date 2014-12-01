e81fab289d29b20eb578371face8b5a73382da2d
Haifeng Li <omycle@gmail.com>
2014-12-01 15:58:58 +0800

    Add Min Heap Sort

--- test.c
+++ test.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int arr[10] = {1, 3, 4, 5, 2, 9, 7, 6, 0, 8};
+int arr[10] = {1, 32, 48, 59, 23, 9, 7, 6, 0, 8};
 void 
 print_array(int array[], int length)
 {
@@ -69,6 +69,79 @@ bubble_sort(int array[], int length)
 	return 0;
 }
 
+int insert_heap_sort(int array[], int i)
+{
+
+	int parent;
+	int tmp;
+	if(i==0)
+		return;	
+	
+	parent = (i-1)/2;
+
+	while(parent >=0){
+		if(array[i]< array[parent])
+		{
+			tmp = array[i];
+			array[i]=array[parent];
+			array[parent] = tmp;		
+		}
+		else{
+			break;
+		}
+		i= parent;
+	        parent = (i - 1)/2;
+	}
+
+	return 0;
+}
+int del_heap_sort(int array[], int length)
+{
+
+	int lchild,rchild,parent,tmp,tmp_value;
+	array[0] = array[length-1];
+	parent = 0;
+	lchild = parent*2 +1;
+	rchild = parent*2 +2;
+	while(lchild < length){
+		if ((lchild<length) && (array[parent] >array[lchild])){
+			if((rchild<length) && array[lchild] > array[rchild]){
+				tmp = rchild;
+			}else{
+				tmp = lchild;
+			}
+		}else if(rchild<length && array[parent] > array[rchild])
+			tmp = rchild;
+		else{
+			break;
+		}
+
+		tmp_value = array[parent];
+		array[parent] = array[tmp];
+		array[tmp] = tmp_value;
+		parent = tmp;
+		
+		lchild = parent*2 +1;
+		rchild = parent*2 +2;		
+	}
+	
+	return 0;
+
+}
+
+int heap_sort(int array[], int length)
+{
+
+	int i=1;
+	for(;i<length;i++)
+		insert_heap_sort(array,i);
+	for(i=10;i>0;i--){
+		printf("%d \n",array[0]);
+		del_heap_sort(array,i);
+	}
+	return 0;
+}
+
 int 
 main()
 {
@@ -76,8 +149,9 @@ main()
 	//scanf("%d", &i);
 	//printf("%d\n", i);
 	//bubble_sort(arr, 10);
-	quick_sort(arr, 0, 9);
+	//quick_sort(arr, 0, 9);
 	//insert_sort(arr, 10);
-	print_array(arr, 10);
+	heap_sort(arr, 10);
+//	print_array(arr, 10);
 	return 0;
 }
