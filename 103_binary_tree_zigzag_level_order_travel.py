3a0b1bdd43c00e57a28d9989dbdbcbce1780a536
Haifeng Li <omycle@gmail.com>
2016-07-16 09:25:54 +0800

    Add some optimization comments.

--- 103_binary_tree_zigzag_level_order_travel.py
+++ 103_binary_tree_zigzag_level_order_travel.py
@@ -29,7 +29,15 @@ class Solution(object):
             if node.left: stack2.append(node.left)
             if node.right:stack2.append(node.right)
 
+	    #len(stack1) is ugly here.
+	    #if not stack1 is graceful. 
             if len(stack1) ==0:
+                """
+                There is not necessary to swap the list.
+                Just set stack2 is empty
+                stack1=stack2
+                stack2=[]
+                """
                 tmp=stack1
                 stack1=stack2
                 stack2=tmp
