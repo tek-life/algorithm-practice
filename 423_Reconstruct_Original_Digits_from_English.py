54f2f2b43fcea19ef40449b63a13be55ebaa8d65
Haifeng Li <omycle@gmail.com>
2016-10-19 15:50:41 +0800

    423. Reconstruct Original Digits from English
    collections is key for travelling string.
    Related operations in collections are important.

---
+++ 423_Reconstruct_Original_Digits_from_English.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def originalDigits(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        import collections
+        
+        cnts=collections.Counter(s)
+        
+        keys=['z','w','u','x','g','r','f','s','o','i']
+        values=['zero','two','four','six','eight','three','five','seven','one','nine']
+        digits=[0,2,4,6,8,3,5,7,1,9]
+        result=[]
+        
+        for i,key in enumerate(keys):
+            num=cnts[key]
+            for k in range(num):
+                result.append(digits[i])
+            for x in values[i]:
+                cnts-=collections.Counter({x:num})
+                    
+        result=sorted(result)
+        return "".join([str(x) for x in result])
